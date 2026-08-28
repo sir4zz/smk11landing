398b53c0afb038560150b015bac3185ccacce6a0
Barrjet <b4rz.maulana@gmail.com>
2026-08-28 13:35:01 +0700

    anu

---
+++ dist/assets/chevron-left-Bv5OnfRH.js
@@ -0,0 +1,1 @@
+import{Rt as e}from"./index-DFvqDd52.js";var t=e(`chevron-left`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]);export{t};
---
+++ dist/assets/contact-AeGnGlBe.js
@@ -0,0 +1,1 @@
+import{Rt as e}from"./index-DFvqDd52.js";var t=e(`contact`,[[`path`,{d:`M16 2v2`,key:`scm5qe`}],[`path`,{d:`M7 21v-2a2 2 0 012-2h6a2 2 0 012 2v2`,key:`k82dct`}],[`path`,{d:`M8 2v2`,key:`pbkmx`}],[`circle`,{cx:`12`,cy:`10`,r:`3`,key:`ilqhr7`}],[`rect`,{x:`3`,y:`3`,width:`18`,height:`18`,rx:`2`,key:`h1oib`}]]);export{t};
